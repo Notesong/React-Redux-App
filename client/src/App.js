79a7a61a0716da4b78e93b1b4f92cd579be4b5a4
Lee Damien <notesong.git@outlook.com>
2020-02-06 22:49:33 -0500

    BeerForm setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import BeerForm from './components/BeerForm';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Brewdog's Beers</h1>
       </header>
+      <section>
+        <BeerForm />
+      </section>
     </div>
   );
 }
---
+++ client/src/components/BeerForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { getData } from "../actions/actions";
+
+const BeerForm = props => {
+  const handleGetData = e => {
+    e.preventDefault();
+    props.getData();
+  };
+
+  return (
+    <>
+      {props.isFetchingData ? (
+        <div>**we are fetching data**</div>
+      ) : (
+        <button onClick={handleGetData}>get data</button>
+      )}
+    </>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    isFetchingData: state.isFetchingData
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getData }
+)(BeerForm);
